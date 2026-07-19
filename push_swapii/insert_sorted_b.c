d88f41c37ebf39f8c99652f9e65a21b441246a8c
marioguzzzman <marioguzzman@gmail.com>
2026-07-20 01:01:09 +0200

    added missing comment

--- push_swapii/insert_sorted_b.c
+++ push_swapii/insert_sorted_b.c
@@ -1,38 +1,45 @@
 /* ************************************************************************** */
 /*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   insert_sorted_b.c                                  :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: dbali <dbali@student.42.fr>                +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/07/13 13:13:35 by dbali             #+#    #+#             */
-/*   Updated: 2026/07/13 14:33:53 by dbali            ###   ########.fr       */
+/*                                                       :::      ::::::::    */
+/*   insert_sorted_b.c                                 :+:      :+:    :+:    */
+/*                                                   +:+ +:+         +:+      */
+/*   By: maguzman <maguzman@student.42.fr>         #+#  +:+       +#+         */
+/*                                               +#+#+#+#+#+   +#+            */
+/*   Created: 2026/07/13 13:13:35 by maguzman         #+#    #+#              */
+/*   Updated: 2026/07/20 00:58:46 by maguzman        ###   ########.fr        */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
 /*
-	insert_sorted_b(): 
-	moves the CURRENT TOP of a onto b, 
-	inserting it at the correct spot so that b stays sorted in DESCENDING order
-		1. save the value at the top of a
-		2. count how many values in b are greater than it
-		3. rotate b until the insertion point reaches the top
-		4. push the value from a to b
+insert_sorted_b():
+moves the CURRENT TOP of a onto b,
+inserting it at the correct spot so that b stays sorted in DESCENDING order
+1. save the value at the top of a
+2. count how many values in b are greater than it
+3. rotate b until the insertion point reaches the top
+4. push the value from a to b
 */
+
 void	insert_sorted_b(t_data *d)
 {
-	long	value; // the value to insert
+	long	value;
+
+	// the value to insert
 	t_stack	*node;
-	int		count_greater; // how many numbers are rgeater than value
-	int		i;
+	int		count_greater;
+
+	// how many numbers are rgeater than value
+	int	i;
 
 	if (!d->a)
 		return ;
-	value = d->a->value; // save the value on top of a
+	value = d->a->value;
+	// save the value on top of a
 	count_greater = 0;
-	node = d->b; // scan b from its top
+	node = d->b;
+	// scan b from its top
 	while (node && node->value > value) // traverse b while current values are greates than the value to insert
 	{
 		count_greater++;
@@ -40,8 +47,9 @@ void	insert_sorted_b(t_data *d)
 	}
 	i = 0;
 	while (i++ < count_greater) // rotate b 'count_greater' times
-		op_rb(d); 
-	op_pb(d); push the top element from a to b
+		op_rb(d);
+	op_pb(d);
+	//push the top element from a to b
 	i = 0;
 	while (i++ < count_greater) // reverse the rotations of b
 		op_rrb(d);
