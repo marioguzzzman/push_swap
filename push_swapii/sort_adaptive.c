a56d3f467683f0e61c3702041599d932bc87e853
marioguzzzman <marioguzzman@gmail.com>
2026-07-23 16:23:26 +0200

    adding small number algo

--- push_swapii/sort_adaptive.c
+++ push_swapii/sort_adaptive.c
@@ -1,20 +1,67 @@
 /* ************************************************************************** */
 /*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   sort_adaptive.c                                    :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: dbali <dbali@student.42.fr>                +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/07/13 14:06:59 by dbali             #+#    #+#             */
-/*   Updated: 2026/07/16 17:09:49 by dbali            ###   ########.fr       */
+/*                                                       :::      ::::::::    */
+/*   sort_adaptive.c                                   :+:      :+:    :+:    */
+/*                                                   +:+ +:+         +:+      */
+/*   By: maguzman <maguzman@student.42.fr>         #+#  +:+       +#+         */
+/*                                               +#+#+#+#+#+   +#+            */
+/*   Created: 2026/07/13 14:06:59 by maguzman         #+#    #+#              */
+/*   Updated: 2026/07/23 16:17:39 by maguzman        ###   ########.fr        */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
+static void	sort_two(t_data *d)
+{
+	if (d->a->value > d->a->next->value)
+		op_sa(d);
+}
+
+static void	sort_three(t_data *d)
+{
+	long	x0;
+	long	x1;
+	long	x2;
+
+	x0 = d->a->value;
+	x1 = d->a->next->value;
+	x2 = d->a->next->next->value;
+	if (x0 > x1)
+		op_sa(d);
+	if (x2 >= x0 && x2 >= x1)
+		return ;
+	if (x2 < x0 && x2 < x1)
+		op_rra(d);
+	else
+	{
+		op_rra(d);
+		op_sa(d);
+	}
+}
+
+static void	sort_small(t_data *d)
+{
+	int	n;
+
+	n = stack_size(d->a);
+	if (n == 2)
+		sort_two(d);
+	else if (n == 3)
+		sort_three(d);
+}
+
 // adaptive_sort: chooses simple/medium/complex based on d->disorder
+
 void	adaptive_sort(t_data *d)
 {
+	if (stack_size(d->a) <= 3)
+	{
+		d->strategy = "adaptive (small)";
+		d->complexity = "O(1)";
+		sort_small(d);
+		return ;
+	}
 	if (d->disorder < 0.2)
 	{
 		d->strategy = "adaptive (simple)";
