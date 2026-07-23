cb2c0b0c8b31575fbb0e4286b0e8a2c0b339d72d
marioguzzzman <marioguzzman@gmail.com>
2026-07-23 14:01:55 +0200

    add helper function to reverse rotate

--- push_swapii/reverse_rotate.c
+++ push_swapii/reverse_rotate.c
@@ -1,27 +1,26 @@
 /* ************************************************************************** */
 /*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   reverse_rotate.c                                   :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: dbali <dbali@student.42.fr>                +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/07/13 13:20:23 by dbali             #+#    #+#             */
-/*   Updated: 2026/07/20 12:06:01 by dbali            ###   ########.fr       */
+/*                                                       :::      ::::::::    */
+/*   reverse_rotate.c                                  :+:      :+:    :+:    */
+/*                                                   +:+ +:+         +:+      */
+/*   By: maguzman <maguzman@student.42.fr>         #+#  +:+       +#+         */
+/*                                               +#+#+#+#+#+   +#+            */
+/*   Created: 2026/07/13 13:20:23 by maguzman         #+#    #+#              */
+/*   Updated: 2026/07/23 14:00:16 by maguzman        ###   ########.fr        */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 #include "libft/libft.h"
 
-// op_rra: reverse rotate a (bottom becomes the top)
-void	op_rra(t_data *d)
+static void	reverse_rotate_stack(t_stack **stack)
 {
 	t_stack	*node;
 	t_stack	*prev;
 
-	if (!d->a || !d->a->next)
+	if (!*stack || !(*stack)->next)
 		return ;
-	node = d->a;
+	node = *stack;
 	prev = NULL;
 	while (node->next)
 	{
@@ -29,68 +28,36 @@ void	op_rra(t_data *d)
 		node = node->next;
 	}
 	prev->next = NULL;
-	node->next = d->a;
-	d->a = node;
+	node->next = *stack;
+	*stack = node;
+}
+
+// op_rra: reverse rotate a (bottom becomes the top)
+
+void	op_rra(t_data *d)
+{
+	reverse_rotate_stack(&d->a);
 	if (!d->mute)
 		ft_putstr_fd("rra\n", 1);
 	d->ops.rra++;
 }
 
 // op_rrb: reverse rotate b (bottom becomes the top)
+
 void	op_rrb(t_data *d)
 {
-	t_stack	*node;
-	t_stack	*prev;
-
-	if (!d->b || !d->b->next)
-		return ;
-	node = d->b;
-	prev = NULL;
-	while (node->next)
-	{
-		prev = node;
-		node = node->next;
-	}
-	prev->next = NULL;
-	node->next = d->b;
-	d->b = node;
+	reverse_rotate_stack(&d->b);
 	if (!d->mute)
 		ft_putstr_fd("rrb\n", 1);
 	d->ops.rrb++;
 }
 
 // op_rrr: rra and rrb at the same time
+
 void	op_rrr(t_data *d)
 {
-	t_stack	*node;
-	t_stack	*prev;
-
-	if (d->a && d->a->next)
-	{
-		node = d->a;
-		prev = NULL;
-		while (node->next)
-		{
-			prev = node;
-			node = node->next;
-		}
-		prev->next = NULL;
-		node->next = d->a;
-		d->a = node;
-	}
-	if (d->b && d->b->next)
-	{
-		node = d->b;
-		prev = NULL;
-		while (node->next)
-		{
-			prev = node;
-			node = node->next;
-		}
-		prev->next = NULL;
-		node->next = d->b;
-		d->b = node;
-	}
+	reverse_rotate_stack(&d->a);
+	reverse_rotate_stack(&d->b);
 	if (!d->mute)
 		ft_putstr_fd("rrr\n", 1);
 	d->ops.rrr++;
